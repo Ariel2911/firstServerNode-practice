8d77c0a0ef186270e9c8d58332749493c41457b1
Ariel Barrios <ariel29111978@hotmail.com>
2023-03-25 17:08:09 -0300

    Crea y muestra el 404.html

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const routes = require("./routes/index.routes");
+const path = require("path"); // Se utiliza para normalizar las rutas de los sistemas operativos
 
 const app = express();
 
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, "../public")))
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname,"../public/404.html"))
+})
 
 app.listen(3000, () => {
   console.log("Servidor a la espera de conexiones en el puerto 3000")
